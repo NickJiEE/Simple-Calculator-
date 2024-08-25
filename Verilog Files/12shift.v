9c7c4c017636bfc57b14f4c085852cabeaac3dbc
Nick Ji <80782918+Unforgiven6@users.noreply.github.com>
2024-08-25 10:14:26 -0700

    Organize

--- Verilog Files/12shift.v
+++ Verilog Files/12shift.v
@@ -3,5 +3,6 @@ module twelve_shift(input data, input clk, input en, input reset, output [11:0] 
     shiftWithEnable shiftWithEnable1(data, clk, en, reset, q[3:0]);
     shiftWithEnable shiftWithEnable2(q[3], clk, en, reset, q[7:4]);
     shiftWithEnable shiftWithEnable3(q[7], clk, en, reset, q[11:8]);
+    
 endmodule
 
--- comb_logic.v
+++
@@ -1,7 +0,0 @@
-module comb_logic(input data, input clk, input en, input reset, output zero, output one, output start); 
-
-zero zero1(data, clk, en, reset, zero);
-one one1(data, clk, en, reset, one);
-start_bit start_bit1(data, clk, en, reset, start);
-
-endmodule
